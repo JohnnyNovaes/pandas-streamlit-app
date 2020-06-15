f735bbd32771a29cd446dc02b4bbf529311a3ea5
João Henrique Saraceni <joao.sarraceni@gmail.com>
2020-06-15 17:10:37 -0300

    Add df.index() in the select box
    Add another function to the select box of General Visualization of Data
    Frames.

--- main.py
+++ main.py
@@ -14,21 +14,34 @@ def main():
               "\n 2) Put the file in the same directory of your project!"
               "\n 3) Make sure name_file is the same name of the file in your directory.")
 
-    # The start of the page
+    # Start of the page
     st.header('PANDAS RESUME')
     # General Information
     st.subheader('General Visualization of Data Frames')
-    # Data Frame Head function:
+    st.text('Provides examples of the basic functions for general visualization of data frames,\nin a '
+            'interactive way.')
+    # Select Box List
+    selection_options = ['List Resume', 'dataFrame.head()', 'dataFrame.index']
+    select = st.selectbox('Choose Function', selection_options)
 
-    selectbox_options = ['Head of Data Frame', 'Numbers of Indexes']
-    select = st.selectbox('Choose Function', selectbox_options)
-    if select == selectbox_options[0]:
+    # Resume List
+    if select == selection_options[0]:
+        st.text(f'1){selection_options[1]} : Returns the first n rows')
+        st.text(f'2){selection_options[2]} : The index (row labels) of the DataFrame.')
+
+    # dataFrame.head()
+    if select == selection_options[1]:
 
         slider = st.slider('Select the number of rows', 1, 890)
         st.dataframe(df.head(slider))
-        st.subheader(f' Pandas Command: ')
-        st.markdown(f'df.head({slider})')
+        st.subheader('Description')
+        st.text(f'Returns the first {slider} rows')
+        st.subheader(f'Command: ')
+        st.text(f'df.head({slider})')
 
+    # dataFrame.index
+    if select == selection_options[2]:
+        st.text(f'{df.index}')
 
 
 
