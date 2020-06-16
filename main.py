f5352b3ed0ed184166296f633a5f23f99d54be04
João Henrique Saraceni <joao.sarraceni@gmail.com>
2020-06-16 14:06:37 -0300

    Add a list of new functions of General Inf of DF
    Add the list of functions:
    dataFrame.isna()
    dataFrame.columns()
    dataFrame.shape

--- main.py
+++ main.py
@@ -4,7 +4,6 @@ import pandas as pd
 
 def main():
     # Importing the csv file
-
     name_file = 'train.csv'
     try:
         df = pd.read_csv(name_file)
@@ -16,21 +15,33 @@ def main():
 
     # Start of the page
     st.header('PANDAS RESUME')
-    # General Information
-    st.subheader('General Visualization of Data Frames')
-    st.text('Provides examples of the basic functions for general visualization of data frames,\nin a '
-            'interactive way.')
+
     # Select Box List
-    selection_options = ['List Resume', 'dataFrame.head()', 'dataFrame.index']
-    select = st.selectbox('Choose Function', selection_options)
+    selection_options = ['Index',
+                         'dataFrame.head()',
+                         'dataFrame.index',
+                         'dataFrame.shape',
+                         'dataFrame.isna()',
+                         'dataFrame.columns']
+
+    select = st.sidebar.selectbox('General Information of DF', selection_options)
 
     # Resume List
     if select == selection_options[0]:
+        # General Information
+        st.subheader('General Information of Data Frames')
+        st.text('Provides examples of the basic functions for general information of data frames,\nin a '
+                'interactive way.')
         st.text(f'1){selection_options[1]} : Returns the first n rows')
         st.text(f'2){selection_options[2]} : The index (row labels) of the DataFrame.')
+        st.text(f'3){selection_options[3]} : Return a tuple representing the dimensionality of the DataFrame.')
+        st.text(f'4){selection_options[4]} : Return a boolean same-sized object indicating if the values are NA.')
+        st.text(f'5){selection_options[5]} : The column labels of the DataFrame.')
 
     # dataFrame.head()
     if select == selection_options[1]:
+        st.header('General Information of Data Frames')
+        st.header("(pandas.DataFrame.head)")
         st.subheader("Result")
         slider = st.slider('Select the number of rows', 1, 890)
         st.dataframe(df.head(slider))
@@ -41,12 +52,51 @@ def main():
 
     # dataFrame.index
     if select == selection_options[2]:
+        st.header('General Information of Data Frames')
+        st.header("(pandas.DataFrame.index)")
+        st.subheader('Description')
+        st.text('The index (row labels) of the DataFrame.')
         st.subheader("Result")
         st.text(f'{df.index}')
         st.dataframe(df.index)
-        st.subheader('Description')
-        st.text('The index (row labels) of the DataFrame.')
+        st.subheader('Command')
+        st.text('dataFrame.index')
 
+    # dataFrame.shape
+    if select == selection_options[3]:
+        st.header('General Information of Data Frames')
+        st.header("(pandas.DataFrame.shape)")
+        st.subheader("Result")
+        st.text(f'{df.shape}')
+        st.dataframe(df.shape)
+        st.subheader('Description')
+        st.text('Return a tuple representing the dimensionality of the DataFrame.')
+        st.subheader("Command")
+        st.text("dataFrame.shape")
+
+    # dataFrame.isna()
+    if select == selection_options[4]:
+        st.header('General Information of Data Frames')
+        st.header("(pandas.DataFrame.isna)")
+        st.subheader("Result")
+        st.text(f'{df.isna()}')
+        st.dataframe(df.isna())
+        st.subheader('Description')
+        st.text('Return a boolean same-sized object indicating if the values are NA.')
+        st.subheader("Command")
+        st.text("dataFrame.isna()")
+
+    # dataFrame.columns()
+    if select == selection_options[5]:
+        st.header('General Information of Data Frames')
+        st.header("(pandas.DataFrame.columns)")
+        st.subheader("Result")
+        st.text(f'{df.columns}')
+        st.dataframe(df.columns)
+        st.subheader('Description')
+        st.text('The column labels of the DataFrame.')
+        st.subheader("Command")
+        st.text("dataFrame.columns")
 
 
 
